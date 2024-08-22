f7b478459092b2d9261fc32786bb506a123b82cb
Saurabh Chamola <saurabh@pearlorganisation.com>
2024-08-22 17:23:31 +0530

    initize repo + add header component

---
+++ src/components/DefaultLayout/Layout/Layout.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "../Header/Header";
+import { Outlet } from "react-router";
+import Footer from "../Footer/Footer";
+
+const Layout = () => {
+  return (
+    <div>
+      <Header />
+
+      <div className="container mx-auto pb-6">
+        <Outlet />
+      </div>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
---
+++ src/pages/ProductListing/ProductListing.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProductListing = () => {
+  return <div>Product Listing Page</div>;
+};
+
+export default ProductListing;
